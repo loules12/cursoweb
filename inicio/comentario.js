4a68661f47f6c48ff5b9a66c0d705a5188c59106
loules12 <basiliobsneto@gmail.com>
2022-04-06 21:02:46 -0300

    Aula 2 + 3

---
+++ inicio/comentario.js
@@ -0,0 +1,12 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log("Passo #03");
+console.log("Passo #01");  // Outro exemplo
+// console.log("Passo #02");
+
+/*
+
+    Este é um 
+    comentário de
+     multiplas linhas
+
+*/
