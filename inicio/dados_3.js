8cc467ad7596999b78fb9e1e1e6bac9c7f15359f
loules12 <basiliobsneto@gmail.com>
2022-04-08 14:31:09 -0300

    Final do inicio

---
+++ inicio/dados_3.js
@@ -0,0 +1,12 @@
+let idade = 26;
+console.log(typeof idade);
+console.log(typeof 26);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estaChovendo = false // ou false
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
---
+++ inicio/desafio_area.js
@@ -0,0 +1,12 @@
+// PI * raio * raio
+
+const PI = 3.14;
+const raio = 10;
+let resultado = pi * raio * raio;
+
+console.log("O valor da área é " + 
+resultado + "m2.");
+
+resultado = Math.PI * raio * raio;
+console.log("O valor da área é " + 
+resultado + "m2.");
