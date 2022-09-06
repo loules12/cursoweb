53496d2a9e0c166e2ba5e673f23c3d3cabf219f5
Basilio Neto <basiliobsneto@gmail.com>
2022-09-06 13:51:38 -0300

    Atualização Node

---
+++ EXERCICIOS-JS/node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,15 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola)
+
+const saudacao = require('saudacao')
+console.log(saudacao.ola)
+
+const s = require('./pastaC/index')
+console.log(s.ola2)
+
+// const http = require('http')
+// const { rest } = require('lodash')
+// http.createServer((req, res) => {
+//     res.white('Bom dia')
+//     res.end()
+// }).listen(8080)
